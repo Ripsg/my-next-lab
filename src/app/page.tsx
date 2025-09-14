996ac0f766d13c26516b182261b1007588230f61
Santiago Garcia <santiago.garcia22@udea.edu.co>
2025-09-14 12:00:05 -0500

    Nuevo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
- return <h1>Hola desde Next.js 🚀</h1>;
+ return <div>
+  <h1>Hola desde Next.js 🚀</h1>
+  <h1>Esto es un nuevo commit 🚀</h1>
+  </div>;
+ 
 }
